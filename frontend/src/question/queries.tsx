d1c75fd34fc7db8312d6dad66ea0aa033756bcb8
ltbrs <lambertbruyas@gmail.com>
2025-04-11 14:16:45 +0200

    checking question

---
+++ frontend/src/question/queries.tsx
@@ -0,0 +1,7 @@
+function isCorrect(userAnswer:string, expectedAnswer:string) {
+    console.log(userAnswer);
+    console.log(expectedAnswer);
+    return userAnswer === expectedAnswer;
+}
+
+export default isCorrect;
