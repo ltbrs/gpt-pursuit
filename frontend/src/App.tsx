c568a41b121a8261f7827df89897558921777b80
lambert.bruyas <lambert.bruyas@ekimetrics.com>
2025-04-11 11:12:34 +0200

    Add data acquisition files and frontend setup for GPT Pursuit application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GPT Pursuit</h1>
+      <p>Welcome to your new React application!</p>
+    </div>
+  )
+}
+
+export default App 
