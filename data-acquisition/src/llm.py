e4c317ac8b410e77c6436305a2bcfd68744c1636
ltbrs <lambertbruyas@gmail.com>
2025-12-14 19:12:47 +0100

    add orchestrator

--- data-acquisition/src/llm.py
+++ data-acquisition/src/llm.py
@@ -6,7 +6,7 @@ PROMPT = """You are participating in a trivia contest. Answer the following ques
 
     
 def get_name(llm_config:LLMConfig) -> str:
-    return llm_config.pipeline_kwargs.get("model", "Unknown")
+    return llm_config['pipeline_kwargs'].get("model", "Unknown")
 
 
 def load_pipeline(llm_config: LLMConfig) -> Pipeline:
@@ -15,12 +15,12 @@ def load_pipeline(llm_config: LLMConfig) -> Pipeline:
     print(model_path)
     try:
         return pipeline(
-            **{**llm_config.pipeline_kwargs, **{"model":model_path}}
+            **{**llm_config["pipeline_kwargs"], **{"model":model_path}}
         )
     except Exception as e:
         print(f"Can't load locally pipeline for {llm_name}: {e}. Downloading it.")
         pipe = pipeline(
-            **llm_config.pipeline_kwargs
+            **llm_config["pipeline_kwargs"]
         )
         pipe.save_pretrained(model_path)
         print(f"Saved pipeline for {llm_name}")
